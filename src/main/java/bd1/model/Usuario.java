1c212b3d24e73d63bcbff0f15ca286d66e8371c3
PH-Palmito <phpalmito@gmail.com>
2025-05-31 15:33:52 -0300

    criei uma parte dos arquivos necessarios

--- src/main/java/bd1/model/Usuario.java
+++ src/main/java/bd1/model/Usuario.java
@@ -1,4 +1,20 @@
 package bd1.model;
 
 public class Usuario {
+    private int id;
+    private String nome;
+    private String email;
+    private String tipo; // "solicitante" ou "gestor"
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
 }
