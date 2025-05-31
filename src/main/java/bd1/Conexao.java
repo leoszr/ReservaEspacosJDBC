2d30848632baa956f8443dd68a097602bef43f05
PH-Palmito <phpalmito@gmail.com>
2025-05-31 15:25:40 -0300

    conexao

---
+++ src/main/java/bd1/Conexao.java
@@ -0,0 +1,15 @@
+package bd1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    private static final String URL = "jdbc:postgresql://localhost:5432/seubanco";
+    private static final String USUARIO = "seuusuario";
+    private static final String SENHA = "suasenha";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
+}
